ace0da383549c6fdcb2f571f9847ae2ebc0a0c7e
waled98 <sadsoul304@gmail.com>
2024-03-14 12:47:31 +0100

    Line-up Seite erstellen und Startseite erstellen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LineUpComponent} from "./line-up/line-up.component";
+import {BaseballScorerHomeComponent} from "./baseball-scorer-home/baseball-scorer-home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: BaseballScorerHomeComponent
+  },
+  {
+    path: 'test', component: LineUpComponent
+  },
+];
---
+++ src/app/baseball-scorer-home/baseball-scorer-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-baseball-scorer-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './baseball-scorer-home.component.html',
+  styleUrl: './baseball-scorer-home.component.css'
+})
+export class BaseballScorerHomeComponent {
+
+}
