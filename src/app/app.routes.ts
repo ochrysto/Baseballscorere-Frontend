dd48ae6e12ae2cf2f2a9ab33329fc7698a8e7574
y.labghough <youness.labghough@gmail.com>
2024-03-14 10:14:00 +0100

    -b BS-41-Create a form for creating referee

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {ScorerAnlegenFormComponent} from "./Components/scorer-anlegen-form/scorer-anlegen-form.component";
+import {
+  SchiedsrichterAnlegenFormComponent
+} from "./Components/schiedsrichter-anlegen-form/schiedsrichter-anlegen-form.component";
 
 export const routes: Routes = [
-  { path: 'scorer_anlegen', component: ScorerAnlegenFormComponent }
+  { path: 'scorer_anlegen', component: ScorerAnlegenFormComponent },
+  { path: 'schiedsrichter_anlegen', component: SchiedsrichterAnlegenFormComponent }
 ];
