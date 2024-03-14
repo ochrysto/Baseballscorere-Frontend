80d1b799adf076d99724c5a694b353ce1697999f
y.labghough <youness.labghough@gmail.com>
2024-03-14 09:37:55 +0100

    -b BS-40-Create a form for creating scorers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ScorerAnlegenFormComponent} from "./Components/scorer-anlegen-form/scorer-anlegen-form.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'scorer_anlegen', component: ScorerAnlegenFormComponent }
+];
