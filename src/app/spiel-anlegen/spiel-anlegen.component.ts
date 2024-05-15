db7358cbb03f1b8fe0815e69880f443adab01057
waled98 <sadsoul304@gmail.com>
2024-05-15 08:45:38 +0200

    Spielanlage implementiert

---
+++ src/app/spiel-anlegen/spiel-anlegen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spiel-anlegen',
+  standalone: true,
+  imports: [],
+  templateUrl: './spiel-anlegen.component.html',
+  styleUrl: './spiel-anlegen.component.css'
+})
+export class SpielAnlegenComponent {
+  constructor() { }
+}
