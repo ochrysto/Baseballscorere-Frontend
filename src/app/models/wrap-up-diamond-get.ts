961c4fb0dc5ff7a347bf0192d6a713cac07f4bbd
Your Name <info@olive-r.eu>
2025-10-29 12:37:43 +0100

    added wrap-up-data for actions and added models accordingly

---
+++ src/app/models/wrap-up-diamond-get.ts
@@ -0,0 +1,10 @@
+export interface WrapUpDiamondGet {
+  order: number;
+  base: number;
+  center: string;
+  first: string;
+  second: string;
+  third: string;
+  home: string;
+  util: string[];
+}
